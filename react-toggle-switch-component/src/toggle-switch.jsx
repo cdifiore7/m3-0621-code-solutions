c025153988b5e24b7031d242dfa4514a10708d76
Chris Difiore <christopher.difiore@gmail.com>
2021-07-06 21:02:24 -0700

    Completed Challenge

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
-import { render } from 'react-dom';
 
 class ToggleSwitch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { toggleOn: true };
+    this.state = { status: false };
     this.handleClick = this.handleClick.bind(this);
   }
-}
 
-handleClick() {
-  this.setState({ toggleOn: !this.state.toggleOn });
-};
+  handleClick() {
+    this.setState({ status: !this.state.status });
+  }
 
-offOn() {
-  if (this.state.toggleOn) {
-    return 'on'
-  } else { return 'off';
-}
-}
+  offOn() {
+    if (this.state.status === false) {
+      return 'on';
+    } else {
+      return 'off';
+    }
+  }
 
-render() {
-  const offOn = this.offOn();
-  const uppercase = offOn.uppercase();
-  return (
+  buttonText() {
+    if (this.state.status === false) {
+      return 'on';
+    } else {
+      return 'off';
+    }
+  }
+
+  render() {
+    const status = this.offOn();
+    const text = this.buttonText();
+    return (
     <div className="toggle-container">
-      <div className={`toggleslider ${status}` onClick={this.handleClick}}>
+      <div className={`toggle-slider ${status}`} onClick={this.handleClick}>
         <div className={`toggle-button ${status}-button`}></div>
     </div>
-    <div>{word}</div>
+    <div>{text}</div>
     </div>
-  )
-}
+    );
+  }
 }
 
 export default ToggleSwitch;

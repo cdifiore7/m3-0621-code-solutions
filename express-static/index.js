502ab259bfc6b530a774592f99856a2fb8709938
Chris Difiore <christopher.difiore@gmail.com>
2021-07-15 18:59:33 -0700

    Exercise Completed

---
+++ express-static/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+var app = express();
+const path = require('path');
+const url = path.join(__dirname, 'public');
+const pathstatic = express.static(url);
+app.use(pathstatic);
+app.listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});

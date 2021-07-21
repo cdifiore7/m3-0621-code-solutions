b8d26f11dd7ba39736396f1c681a3a5aad6ed2f0
Chris Difiore <christopher.difiore@gmail.com>
2021-07-20 19:26:02 -0700

    Fixes Made

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -21,8 +21,8 @@ export default class App extends React.Component {
     fetch('api/todos')
       .then(res => res.json())
       .then(data => this.setState({ todos: data })
-  );
-}
+      );
+  }
   /**
      * Use fetch to send a GET request to `/api/todos`.
      * Then 😉, once the response JSON is received and parsed,
@@ -37,14 +37,14 @@ export default class App extends React.Component {
       },
       body: JSON.stringify(newTodo)
     })
-    .then(res => res.json())
-    .then(newEntry => {
-      const updatedToDo = [...this.state.todos];
-      updatedToDo.push(newEntry);
-      this.setState({ todos: updatedToDo });
-    });
-  };
-    /**
+      .then(res => res.json())
+      .then(newEntry => {
+        const updatedToDo = [...this.state.todos];
+        updatedToDo.push(newEntry);
+        this.setState({ todos: updatedToDo });
+      });
+  }
+  /**
     * Use fetch to send a POST request to `/api/todos`.
     * Then 😉, once the response JSON is received and parsed,
     * add the created todo to the state array.
@@ -59,10 +59,9 @@ export default class App extends React.Component {
     * of the old array, plus the object returned by the server.
     */
 
-
   toggleCompleted(todoId) {
     let matchingToDo;
-    for(let i = 0; i < this.state.todos.length; i++) {
+    for (let i = 0; i < this.state.todos.length; i++) {
       const currentToDo = this.state.todos[i];
       if (currentToDo.todoId === todoId) {
         matchingToDo = i;
@@ -77,17 +76,16 @@ export default class App extends React.Component {
       },
       body: JSON.stringify({ isCompleted: isCompletedstatus })
     })
-    .then(response => response.json())
-    .then(data => {
-      const newTodo = {...this.state.todos};
-      newTodo[matchingToDo] = data;
-      this.setState({ todos: newTodo});
-    })
-    .catch(error => {
-      console.error('error:', error)
-    };
-}
-    /**
+      .then(response => response.json())
+      .then(data => {
+        const newTodo = { ...this.state.todos };
+        newTodo[matchingToDo] = data;
+        this.setState({ todos: newTodo });
+      })
+      .catch(error => {
+        console.error('error:', error);
+      }
+        /**
      * Find the index of the todo with the matching todoId in the state array.
      * Get its "isCompleted" status.
      * Make a new object containing the opposite "isCompleted" status.
@@ -103,7 +101,9 @@ export default class App extends React.Component {
      *
      * TIP: Be sure to SERIALIZE the updates in the body with JSON.stringify()
      * And specify the "Content-Type" header as "application/json"
-     */
+  */);
+  }
+
   render() {
     return (
       <div className="container">

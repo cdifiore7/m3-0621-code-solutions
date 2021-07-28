ecf15e2c470cf468a69aaaa0e60deb1f5f341aeb
Chris Difiore <christopher.difiore@gmail.com>
2021-07-27 19:41:21 -0700

    Progress Made

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,6 +3,25 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {Index: 0};
+    this.handleClickRight = this.handleClickRight.bind(this);
+    this.handleClickLeft = this.handleClickLeft.bind(this);
+    this.handleDotClick = this.handleDotClick.bind(this);
+    this.resetterClick = this.resetterClick.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  };
+  handleDotClick(event) {
+    this.setState({
+      Index: (event.target.tagname === 'li')
+      ? parseInt(event.target.id, 10)
+      : parseInt(event.target.closest('li').id, 10)
+    })
+    this.resetterClick();
+    this.componentDidMount();
   }
-}
-export default Carousel;
+  componentDidMount(event) {
+    this.timer = setInterval(() => this.handleClickRight(), 3000);
+  };
+  handleClickRight(event) {
+    this.setState({ Index : ((this.state.Index + this.props.length))}) +w
+  }

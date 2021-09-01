fccc963701937615255e46a75c4b35a1580cfc0f
Chris Difiore <christopher.difiore@gmail.com>
2021-08-31 20:28:35 -0700

    Exercise Completed

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -1,27 +1,94 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {Index: 0};
-    this.handleClickRight = this.handleClickRight.bind(this);
-    this.handleClickLeft = this.handleClickLeft.bind(this);
-    this.handleDotClick = this.handleDotClick.bind(this);
-    this.resetterClick = this.resetterClick.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-  };
-  handleDotClick(event) {
+    this.state = { currentIndex: 0 };
+    this.autoStepper = this.autoStepper.bind(this);
+    this.autoResetter = this.autoResetter.bind(this);
+    this.clickLeftArrow = this.clickLeftArrow.bind(this);
+    this.clickRightArrow = this.clickRightArrow.bind(this);
+    this.dotClick = this.dotClick.bind(this);
+  }
+
+  clickLeftArrow(event) {
     this.setState({
-      Index: (event.target.tagname === 'li')
-      ? parseInt(event.target.id, 10)
-      : parseInt(event.target.closest('li').id, 10)
-    })
-    this.resetterClick();
-    this.componentDidMount();
+      currentIndex: ((this.state.currentIndex +
+      this.props.items.length) - 1) %
+       this.props.items.length
+    });
+    this.autoResetter();
+    this.autoStepper();
   }
-  componentDidMount(event) {
-    this.timer = setInterval(() => this.handleClickRight(), 3000);
-  };
-  handleClickRight(event) {
-    this.setState({ Index : ((this.state.Index + this.props.length))}) +w
+
+  clickRightArrow(event) {
+    this.setState({
+      currentIndex:
+      ((this.state.currentIndex + this.props.items.length) + 1) %
+      this.props.items.length
+    });
+    this.autoResetter();
+    this.autoStepper();
   }
+
+  clickDot(event) {
+    this.setState({
+      currentIndex:
+      (event.target.tagName === 'li')
+        ? parseInt(event.target.id, 10)
+        : parseInt(event.target.closest('li').id, 10)
+    });
+    this.autoResetter();
+    this.autoStepper();
+  }
+
+  componentDidMount() {
+    this.autoStepper();
+  }
+
+  autoStepper() {
+    this.timer = setInterval(
+      () => this.setState({
+        currentIndex: ((this.state.currentIndex + this.props.items.length) + 1) %
+        this.props.items.length
+      }),
+      3000
+    );
+  }
+
+  autoResetter() {
+    clearInterval(this.timer);
+  }
+
+  render(props) {
+    const pokeList = this.props.items.map((element, index) => {
+      return (
+          <li key={element.index} id={index} onClick={this.dotClick}>
+            {(this.state.currentIndex === index)
+              ? <i className="fas fa-circle"></i>
+              : <i className="far fa-circle"></i>}
+          </li>
+      );
+    });
+    return (
+        <div className="carousel">
+          <div onClick={this.clickLeftArrow} className="left-arrow">
+            <i className="fas fa-chevron-left"></i>
+          </div>
+          <div className="carousel-body">
+            <div className="carousel-image-container">
+              <img src={this.props.items[this.state.currentIndex].source}
+              alt={this.props.items[this.state.currentIndex].name} />
+            </div>
+            <ul className='index'>{pokeList}</ul>
+          </div>
+          <div onClick={this.clickRightArrow} className="right-arrow">
+            <i className="fas fa-chevron-right"></i>
+          </div>
+        </div>
+    );
+  }
+}
+export default Carousel;
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './carousel';
 
-ReactDOM.render(
-  <Carousel />,
-  document.querySelector('#root')
-);
+const PokeArray = [
+  {
+    index: 1,
+    name: 'Bulbasaur',
+    source: './images/bulbasaur.jpg'
+  },
+  {
+    index: 2,
+    name: 'Mewtwo',
+    source: './images/Mewtwo.png'
+  },
+  {
+    index: 3,
+    name: 'Squirtle',
+    source: './images/squirtle.png'
+  },
+  {
+    index: 4,
+    name: 'Pikachu',
+    source: './images/pikachu.png'
+  }
+
+];
+
+ReactDOM.render(<Carousel items={PokeArray} />, document.getElementById('root'));
